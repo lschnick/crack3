3bd77be758ad65954c70458064dd0e159eecfa44
lschnick <lschnick@student.sierracollege.edu>
2025-05-06 11:52:45 -0700

    First commit

--- crack.c
+++ crack.c
@@ -11,6 +11,9 @@
 #define PASS_LEN 50     // Maximum length any password will be.
 #define HASH_LEN 33     // Length of hash plus one for null.
 
+char (*loadFile2D(char *filename, int *size))[HASH_LEN];
+char *substringSearch2D(char *target, char (*lines)[HASH_LEN], int size);
+int alphabetic(const void *a, const void *b);
 
 int main(int argc, char *argv[])
 {
@@ -24,16 +27,46 @@ int main(int argc, char *argv[])
     //   Use either a 2D array or an array of arrays.
     //   Use the loadFile function from fileutil.c
     //   Uncomment the appropriate statement.
-    int size;
-    //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
-    //char **hashes = loadFile(argv[1], &size);
+    int size, count, length;
+    count = 0;
+    char (*hashes)[HASH_LEN] = loadFile2D(argv[1], &size); //create 2d array of hashes from hash file
     
     // CHALLENGE1: Sort the hashes using qsort.
-    
-    // TODO
-    // Open the password file for reading.
+    qsort(hashes, size, sizeof(char[HASH_LEN]), alphabetic); //sort list of hashes alphabetically
 
     // TODO
+    // Open the password file for reading.
+    // TODO
+    char dictln[1000];
+    // Open the dictionary file for reading.
+    FILE *dictf = fopen(argv[2], "r");
+     if (dictf == NULL)
+    {
+        printf("error with dict file");
+        exit(1);
+    }
+
+    while ((fgets(dictln, 1000, dictf))!= NULL) //test for eof
+    {
+        length = strlen(dictln);
+        int i = length - 1;
+        if ( dictln[i] == '\n') //trim new line char
+        {
+            dictln[i] = '\0';
+        }
+        length = strlen(dictln);
+        char *hashed = md5(dictln, length); //hash dict word using md5
+        char *found = substringSearch2D(hashed, hashes, size); //use strcmp to check for matches
+        if (found)
+        {
+			printf("Found: %s\n", found);
+            count++;
+        }
+		else
+        {
+			printf("Not found!\n");
+        }
+    }
     // For each password, hash it, then use the array search
     // function from fileutil.h to find the hash.
     // If you find it, display the password and the hash.
@@ -43,6 +76,16 @@ int main(int argc, char *argv[])
     // TODO
     // When done with the file:
     //   Close the file
+    fclose(dictf);
     //   Display the number of hashes found.
+    printf("Hashes found: %d\n", count);
     //   Free up memory.
+    free(hashes);
+}
+
+int alphabetic(const void *a, const void *b) //check alphebetic order for qsort
+{
+    char *aa = (char*)a;
+    char *bb = (char*)b;
+    return strcmp(aa, bb);
 }
---
+++ fileutil.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "fileutil.h"
+
+char (*loadFile2D(char *filename, int *size))[COLS];
+char *substringSearch2D(char *target, char (*lines)[COLS], int size);
+void free2D(char (*arr)[COLS]);
+
+// DIRECTIONS
+// Choose whether you are doing the 2D array or
+// the array of arrays.
+// For the 2D array,
+//    implement loadFile2D, substringSearch2D, and free2D.
+// For the array of arrays, 
+//    implement loadFileAA, substringSearchAA, and freeAA.
+
+char (*loadFile2D(char *filename, int *size))[COLS]
+{
+	FILE *in = fopen(filename, "r");
+	if (!in)
+	{
+	    perror("Can't open file");
+	    exit(1);
+	}
+	
+	// TODO
+	// Allocate memory for an 2D array, using COLS as the width.
+	char (*strs)[COLS]= malloc(10 * sizeof(char[COLS]));
+	// Read the file line by line into a buffer.
+	*size = 0;
+	char ln[1000]; //buffer line, max 1000 characters
+	int rows, i, capacity;
+	rows = 0; //counts the rows in use
+	capacity = 10; //counts the total rows avalible
+	while ((fgets(ln, 1000, in))!= NULL) //tests for eof
+    {
+        int length = strlen(ln);
+        int i = length - 1;
+        if ( ln[i] == '\n') //trim new ln character
+        {
+            ln[i] = '\0';
+        }
+		//   Trim newline.
+		//   Expand array if necessary (realloc).
+		if (*size >= capacity)  //tests for max capacity
+		{
+			capacity = capacity * 1.5; //increaases capacity by %150
+			strs = realloc(strs, capacity * sizeof(char[COLS]));
+		}
+		strcpy(strs[rows], ln); //puts buffer into 2D array
+		rows++;
+		(*size)++; //notes active rows
+	}
+	//   Copy each line from the buffer into the array (use strcpy).
+    // Close the file.
+	fclose(in);
+	// The size should be the number of entries in the array.
+	
+	// Return pointer to the array.
+	return strs;
+}
+
+// Search the array for the target string.
+// Return the found string or NULL if not found.
+
+
+char * substringSearch2D(char *target, char (*lines)[COLS], int size)
+{
+	int i;
+    for (i = 0; i < size; i++) //loop through each row of 2D array for target string
+	{
+		if (strcmp(lines[i], target) == 0) 
+		{
+			return lines[i]; //return string if it matches
+		}
+	}
+    return NULL; //else return null
+}
+
+
+
+void free2D(char (*arr)[COLS])
+{
+	free(arr); //frees 2D array storage
+}
